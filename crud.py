cdf936d199113f086028db0406b5dceaee497c2e
Tamara Vashchuk <ms.vashchuk96@gmail.com>
2023-03-07 21:50:37 -0800

    Create function that returns total count of messages per day per user

--- crud.py
+++ crud.py
@@ -1,6 +1,8 @@
 """CRUD operations. """
+from datetime import datetime
 from typing import List
-
+from sqlalchemy import func
+from sqlalchemy import Date
 from model import db, Message, Member, connect_to_db
 
 
@@ -17,7 +19,25 @@ def search_members(search_value) -> List[Member]:
                                (Member.member_name.like(f'%{search_value}%'))).all()
 
 
+def get_msg() -> List[Message]:
+    """Return all users."""
+    return Message.query.all()
+
+
+def mes_per_day_per_user():
+    """Function returns total counts per user per day"""
+    query = db.session.query(
+        func.count().label('cnt'),
+        func.to_timestamp(Message.date).cast(Date).label('day'),
+        Message.member_id
+    ).group_by('day', 'member_id')
+
+    result = query.all()
+    return result
+
+
 if __name__ == "__main__":
     from server import app
 
     connect_to_db(app)
+    print("Connected to DB.")
